132e3dfeecc761505d6b35c73b8e7664288d70bf
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-03 00:29:04 +0900

    Layout finished

--- app/src/main/java/com/gardenlab/doitmission9/MainActivity.java
+++ app/src/main/java/com/gardenlab/doitmission9/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    InfoFragment infoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        infoFragment = new InfoFragment();
+
+        getSupportFragmentManager().beginTransaction().add(R.id.container, infoFragment).commit();
+
     }
 }
