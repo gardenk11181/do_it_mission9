8803e7c978f4dad31f73610ea984b61d8cf46915
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-03 01:33:01 +0900

    DatePickerDialog added

--- app/src/main/java/com/gardenlab/doitmission9/InfoFragment.java
+++ app/src/main/java/com/gardenlab/doitmission9/InfoFragment.java
@@ -1,19 +1,56 @@
 package com.gardenlab.doitmission9;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class InfoFragment extends Fragment{
+    Button btnBirth;
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.info_fragment, container, false);
+        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.info_fragment, container, false);
+
+
+        btnBirth = rootView.findViewById(R.id.btn_birth);
+        btnBirth.setText(dateFormat.format(new Date()));
+
+        btnBirth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dialog = new DatePickerDialog(getActivity());
+                dialog.show();
+                dialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Calendar cal = Calendar.getInstance();
+                        cal.set(year,month, dayOfMonth);
+                        Date selected = cal.getTime();
+                        btnBirth.setText(dateFormat.format(selected));
+                    }
+                });
+            }
+        });
+
+        return rootView;
     }
 }
--- app/src/main/java/com/gardenlab/doitmission9/MainActivity.java
+++ app/src/main/java/com/gardenlab/doitmission9/MainActivity.java
@@ -1,11 +1,25 @@
 package com.gardenlab.doitmission9;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     InfoFragment infoFragment;
+    EditText editName;
+    EditText editAge;
+    EditText editBirth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +27,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         infoFragment = new InfoFragment();
-
         getSupportFragmentManager().beginTransaction().add(R.id.container, infoFragment).commit();
+
 
     }
 }
+
+
